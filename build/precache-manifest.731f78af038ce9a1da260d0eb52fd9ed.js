480ed783c550aaf3f00088e5ae4392e0aea86ffe
libin1991 <3120217729@qq.com>
2020-05-02 19:06:57 +0800

    add demo code

---
+++ build/precache-manifest.731f78af038ce9a1da260d0eb52fd9ed.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cf27efc4aea14d63ba9",
+    "url": "/static/js/main.1f4d8c5a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ef19ec5a07b80e30ed8b",
+    "url": "/static/js/2.c2980253.chunk.js"
+  },
+  {
+    "revision": "2e353be51db26bf8c0c5ca1594ab9472",
+    "url": "/index.html"
+  }
+];
